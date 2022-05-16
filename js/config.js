7ca7cb98175fa75e8d62157f9e1159ebaa3edc3d
BruceBit <brucebit12@gmail.com>
2022-05-15 21:25:47 -0500

    user management

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'us-west-1_IaPDoME4S', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '40k5aqa1es7vc4045e1r3q4nlo', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: 'us-west-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
